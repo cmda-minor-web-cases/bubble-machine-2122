c9513eebb7b19343f766137bd3f6ad39dee50ca2
Remco <remco.kuckelkorn@soverin.net>
2022-06-01 14:02:11 +0200

    persons are bigger

--- public/js/D3-graph.js
+++ public/js/D3-graph.js
@@ -19,13 +19,24 @@ const update = async (data) => {
   const nodes = data.nodes
   const links = data.links
 
+  // const linkGen = d3.linkRadial()
+
   xScale.domain([d3.min(nodes, (d) => d.x), d3.max(nodes, (d) => d.x)])
   yScale.domain([d3.min(nodes, (d) => d.y), d3.max(nodes, (d) => d.y)])
+
+  // const path = svg.selectAll('path').data(links).join(
+  //   (enter) => {
+  //     enter = enter.append('path')
+  //       .attr('d', linkGen)
+  //     return enter
+  //   },
+  //   (update) => update,
+  //   (exit) => exit.remove() 
+  // )
 
   const circle = svg.selectAll('circle').data(nodes).join(
     (enter) => {
       enter = enter.append('circle')
-        .attr('r', `${size}px`)
       return enter
     },
     (update) => update,
@@ -36,7 +47,19 @@ const update = async (data) => {
     .transition()
     .attr('cx', (nodes) => xScale(nodes.x))
     .attr('cy', (nodes) => yScale(nodes.y))
+    .attr('r', (nodes) => {
+      if (nodes.label === 'person'){
+        return '15px'
+      } else {
+        return '5px'
+      }
+    })
     .attr('class', (nodes) => nodes.label )
+  
+  // path
+  //   .transition()
+  //   .attr('fill', 'none')
+  //   .attr('class', (links) => links.label )
 }
 
 export default update
