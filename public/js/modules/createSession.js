7c45e81ec2b912a5ed5471445b7d7fea9ccbaedf
nora kramer <nora.kramer2@hva.nl>
2022-06-03 12:34:20 +0200

    layout updates

--- public/js/modules/createSession.js
+++ public/js/modules/createSession.js
@@ -1,8 +1,10 @@
-import { fetchAPI } from './script.js'
-import updateGraph from './D3-graph.js'
+import updateGraph from "../D3-graph.js"
+import { fetchDataFromAPI } from "./apiData.js"
+
+
 
 export const createSession = async (sessionID) => {
-  fetchAPI('POST', 'https://bubble-machine-api-dummy.herokuapp.com/rest/session')
-  const data = fetchAPI('GET', `https://bubble-machine-api-dummy.herokuapp.com/rest/session/${sessionID}`)
+  fetchDataFromAPI('POST', 'https://bubble-machine-api-dummy.herokuapp.com/rest/session')
+  const data = fetchDataFromAPI('GET', `https://bubble-machine-api-dummy.herokuapp.com/rest/session/${sessionID}`)
   updateGraph(await data)
 }
--- public/js/modules/updateSession.js
+++ public/js/modules/updateSession.js
@@ -1,8 +1,9 @@
-import { fetchAPI } from './script.js'
-import updateGraph from './D3-graph.js'
+
+import updateGraph from '../D3-graph.js'
+import { fetchDataFromAPI } from './apiData.js'
 
 export const nextStep = async (sessionID) => {
-  fetchAPI('POST', `https://bubble-machine-api-dummy.herokuapp.com/rest/session/${sessionID}/step`)
-  const data = await fetchAPI('GET', `https://bubble-machine-api-dummy.herokuapp.com/rest/session/${sessionID}`)
+  fetchDataFromAPI('POST', `https://bubble-machine-api-dummy.herokuapp.com/rest/session/${sessionID}/step`)
+  const data = await fetchDataFromAPI('GET', `https://bubble-machine-api-dummy.herokuapp.com/rest/session/${sessionID}`)
   updateGraph(await data)
 }
